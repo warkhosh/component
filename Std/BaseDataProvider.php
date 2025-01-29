beb45f74240f6110701173df89db7c383fb12c41
ekv <ekv.programmer@gmail.com>
2025-01-29 15:58:46 +0300

    feat: добавил в проект warkhosh/exception
    ExceptionDataProvider.php теперь устаревший и подлежит замене на ImprovedException!

--- Std/BaseDataProvider.php
+++ Std/BaseDataProvider.php
@@ -4,6 +4,7 @@ namespace Warkhosh\Component\Std;
 
 use Warkhosh\Component\Collection\Interfaces\Arrayable;
 use Warkhosh\Component\Collection\Interfaces\Jsonable;
+use Warkhosh\Exception\ImprovedException;
 use ArrayObject;
 use JsonSerializable;
 use Traversable;
@@ -600,7 +601,7 @@ class BaseDataProvider extends ArrayObject implements Arrayable, DataProviderInt
             $this->data['exception_line'] = $exception->getLine();
             $this->data['exception_trace'] = $exception->getTraceAsString();
 
-            if ($exception instanceof ExceptionDataProvider) {
+            if ($exception instanceof ImprovedException) {
                 $field = $exception->getField();
                 $this->data['field'] = empty($field) ? null : $field;
 
--- Std/ExceptionDataProvider.php
+++ Std/ExceptionDataProvider.php
@@ -9,6 +9,8 @@ use Throwable;
  * ExceptionDataProvider
  *
  * Класс для своих исключений с дополнительными полями $field, $signal, $system
+ *
+ * @deprecated заменить на ImprovedException из warkhosh/exception!
  */
 class ExceptionDataProvider extends Exception implements Throwable
 {
